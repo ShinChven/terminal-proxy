36aa699303c90a885254da6074c07aa09eb40f86
ShinChven <shinchven@gmail.com>
2018-10-25 00:42:01 +0800

    fix path

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ fs.readFile(homedir+'/.termproxy', 'utf-8').then(data => {
             const platformName = process.platform;
             let scriptAccordingToPlatform = null;
             if (platformName === 'darwin' || platformName === 'linux') { // use bash if running on macOS or Linux
-                scriptAccordingToPlatform = "bash ./src/proxy.sh";
+                scriptAccordingToPlatform = "bash "+__dirname+"/proxy.sh";
             } else if (platformName === 'win32') { // use Batch script if running on Windows
                 scriptAccordingToPlatform = __dirname+'/proxy.bat';
             } else { // end it if unsupported platform found.
